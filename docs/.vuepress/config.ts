08d7b3ae18ab964a1e37c3daebc09917d085e21e
jiypa <3394521327@qq.com>
2024-07-30 14:38:35 +0800

    update: 更新 vuepress 配置文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,8 @@ import { defineUserConfig, defaultTheme } from 'vuepress';
 import { searchPlugin } from '@vuepress/plugin-search';
 
 export default defineUserConfig({
-    lang: 'zh-CN',
+    base: '/',
+    lang: 'zh-Hans',
     title: '前端面试宝典',
     description: '全网最全前端面试题总结',
     head: [
@@ -15,33 +16,34 @@ export default defineUserConfig({
             {
                 text: '基础',
                 children: [
-                    { text: 'HTML', link: '/html/' },
-                    { text: 'CSS', link: '/css/' },
-                    { text: 'JS', link: '/js/' },
-                    { text: 'TS', link: '/ts/' },
+                    { text: 'HTML', link: '/basics/html/' },
+                    { text: 'CSS', link: '/basics/css/' },
+                    { text: 'JS', link: '/basics/js/' },
+                    { text: 'TS', link: '/basics/ts/' },
+                    { text: 'Node', link: '/basics/node/' },
                 ],
             },
             {
                 text: '框架',
                 children: [
-                    { text: 'Vue', link: '/vue/' },
-                    { text: 'React', link: '/react/' },
+                    { text: 'Vue', link: '/frameworks/vue/' },
+                    { text: 'React', link: '/frameworks/react/' },
                 ],
             },
             {
                 text: '工具',
                 children: [
-                    { text: 'Webpack', link: '/webpack/' },
-                    { text: 'Git', link: '/git/' },
+                    { text: 'Webpack', link: '/tools/webpack/' },
+                    { text: 'Git', link: '/tools/git/' },
                 ],
             },
             {
-                text: '网络',
-                link: '/network/',
-            },
-            {
-                text: '算法',
-                link: '/algorithm/',
+                text: '其他',
+                children: [
+                    { text: '网络', link: '/others/network/' },
+                    { text: '安全', link: '/others/security/' },
+                    { text: '算法', link: '/others/algorithm/' },
+                ],
             },
             {
                 text: '友链',
@@ -55,50 +57,59 @@ export default defineUserConfig({
             },
         ],
         sidebar: {
-            '/html/': [{
-                text: 'HTML',
-                children: ['/html/README.md'],
+            '/basics/': [{
+                text: '基础',
+                children: [
+                    '/basics/html/README.md',
+                    '/basics/css/README.md',
+                    '/basics/js/README.md',
+                    '/basics/ts/README.md',
+                    '/basics/node/README.md',
+                ],
             }],
-            '/css/': [{
-                text: 'CSS',
-                children: ['/css/README.md'],
+            '/frameworks/': [{
+                text: '框架',
+                children: [
+                    '/frameworks/vue/README.md',
+                    '/frameworks/react/README.md',
+                ],
             }],
-            '/js/': [{
-                text: 'JavaScript',
-                children: ['/js/README.md'],
+            '/tools/': [{
+                text: '工具',
+                children: [
+                    '/tools/webpack/README.md',
+                    '/tools/git/README.md',
+                ],
             }],
-            '/ts/': [{
-                text: 'TypeScript',
-                children: ['/ts/README.md'],
-            }],
-            '/vue/': [{
-                text: 'Vue',
-                children: ['/vue/README.md'],
-            }],
-            '/react/': [{
-                text: 'React',
-                children: ['/react/README.md'],
-            }],
-            '/webpack/': [{
-                text: 'Webpack',
-                children: ['/webpack/README.md'],
-            }],
-            '/git/': [{
-                text: 'Git',
-                children: ['/git/README.md'],
-            }],
-            '/network/': [{
-                text: '网络',
-                children: ['/network/README.md'],
-            }],
-            '/algorithm/': [{
-                text: '算法',
-                children: ['/algorithm/README.md'],
+            '/others/': [{
+                text: '其他',
+                children: [
+                    '/others/network/README.md',
+                    '/others/security/README.md',
+                    '/others/algorithm/README.md',
+                ],
             }],
         },
-        editLink: false,
-        lastUpdated: false,
-        contributors: false,
+        // page meta
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+        // 404 page
+        notFound: [
+            '这里什么都没有',
+            '我们怎么到这来了？',
+            '这是一个 404 页面',
+            '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleColorMode: '切换颜色模式',
+        toggleSidebar: '切换侧边栏',
     }),
     plugins: [
         searchPlugin({}),
